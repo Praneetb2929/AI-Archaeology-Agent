70247df9c16535b49720bf5983544d82612ff259
Praneet Biswal <144131975+Praneetb2929@users.noreply.github.com>
2025-09-21 20:49:48 +0530

    Update MapView.js

--- components/MapView.js
+++ components/MapView.js
@@ -1,19 +1,21 @@
-// components/MapView.js
+"use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function MapView({ anomalies }) {
+export default function MapView({ markers = [] }) {
   return (
-    <MapContainer center={[20, 77]} zoom={5} style={{ height: "400px", width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {anomalies.map((a, idx) => (
-        <Marker key={idx} position={[a.latitude, a.longitude]}>
-          <Popup>
-            <strong>{a.anomaly_type}</strong>
-            <p>{a.interpretation}</p>
-          </Popup>
-        </Marker>
-      ))}
+    <MapContainer center={[20.5937, 78.9629]} zoom={5} className="h-[400px] w-full rounded shadow">
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="&copy; OpenStreetMap contributors"
+      />
+      {Array.isArray(markers) &&
+        markers.map((marker) => (
+          <Marker key={marker.id} position={marker.position}>
+            <Popup>{marker.text}</Popup>
+          </Marker>
+        ))}
     </MapContainer>
   );
 }
+
